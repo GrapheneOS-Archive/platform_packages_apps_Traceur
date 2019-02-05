811bff7c3e65076481d7c8bc8ec86bc6bc4379ec
Carmen Jackson <carmenjackson@google.com>
2019-02-05 14:11:42 -0800

    Add notify_traceur shell script to be called from traceur.rc.
    In order to allow us to have more specific selinux policies around
    this am call, move it into a shell script instead of calling it directly
    from the .rc file.
    
    Bug: 116754134
    Test: manual
    
    Change-Id: Ice3ca935f401946d16d3e95b0081873086156ef9

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ LOCAL_PACKAGE_NAME := Traceur
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_USE_AAPT2 := true
+LOCAL_REQUIRED_MODULES := notify_traceur.sh
 
 LOCAL_PROGUARD_FLAG_FILES += proguard.flags
 
@@ -26,6 +27,14 @@ LOCAL_INIT_RC := traceur.rc
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := notify_traceur.sh
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+
+include $(BUILD_PREBUILT)
 
 # Use the following include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
