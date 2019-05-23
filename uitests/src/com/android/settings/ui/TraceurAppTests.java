3edb6d90b1abeba00b914e339f54f55254c6803a
Carmen Jackson <carmenjackson@google.com>
2019-05-23 15:44:27 -0700

    Update Traceur tests to work well on smaller devices.
    There are too many UI elements on the screen to all be visible at once
    on a walleye, so the test fails when I run it locally on this device.
    Adding a scroll to the middle of the UI check test to make this test
    more robust.
    
    Test: atest TraceurUiTests works locally on walleye now.
    Change-Id: I396a137520107e48034dca6ff75abb390282694d

--- uitests/src/com/android/settings/ui/TraceurAppTests.java
+++ uitests/src/com/android/settings/ui/TraceurAppTests.java
@@ -24,6 +24,8 @@ import android.os.RemoteException;
 import android.platform.test.annotations.Presubmit;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.Until;
 
 import androidx.test.InstrumentationRegistry;
@@ -100,6 +102,10 @@ public class TraceurAppTests {
         assertNotNull("Per-CPU buffer size element not found.",
                 mDevice.wait(Until.findObject(By.text("Per-CPU buffer size")),
                 TIMEOUT));
+
+        UiScrollable mainScreen = new UiScrollable(new UiSelector().scrollable(true));
+        mainScreen.scrollToEnd(2);
+
         assertNotNull("Clear saved traces element not found.",
                 mDevice.wait(Until.findObject(By.text("Clear saved traces")),
                 TIMEOUT));
